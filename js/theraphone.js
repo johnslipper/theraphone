b95ea6394d14dfb998e9ae6af3c2be2bed678900
John Slipper <jslipper@me.com>
2016-07-27 17:12:06 +1200

    iOS Safe Audio Context Externalised

--- js/theraphone.js
+++ js/theraphone.js
@@ -1,3 +1,4 @@
+import createAudioContext from 'ios-safe-audio-context';
 class TheraPhone {
   constructor(
       config = {
@@ -38,30 +39,6 @@ class TheraPhone {
     // return new window.AudioContext()
 
     // iOS fixed audio context
-    var AudioCtor = window.AudioContext || window.webkitAudioContext
-
-    desiredSampleRate = typeof desiredSampleRate === 'number'
-      ? desiredSampleRate
-      : 44100
-    var context = new AudioCtor()
-
-    // Check if hack is necessary. Only occurs in iOS6+ devices
-    // and only when you first boot the iPhone, or play a audio/video
-    // with a different sample rate
-    if (/(iPhone|iPad)/i.test(navigator.userAgent) &&
-        context.sampleRate !== desiredSampleRate) {
-      var buffer = context.createBuffer(1, 1, desiredSampleRate)
-      var dummy = context.createBufferSource()
-      dummy.buffer = buffer
-      dummy.connect(context.destination)
-      dummy.start(0)
-      dummy.disconnect()
-
-      context.close() // dispose old context
-      context = new AudioCtor()
-    }
-
-    return context
   }
 
   // Create default gain node
